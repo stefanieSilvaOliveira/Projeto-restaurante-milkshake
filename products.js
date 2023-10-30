c4da3722de98cc2e1ea301f0ea4d7cab84933eee
bonecahStefanie <stefaniesilvadeoliveira@yahoo.com.br>
2023-10-30 11:54:14 -0300

    index-html

---
+++ products.js
@@ -0,0 +1,7 @@
+const menuOptions = [
+    { name: 'Coffe Moranguete', price: 27, src: './img/morango.png' },
+    { name: 'Coffe Moranguete', price: 25, src: './img/coffe-chantily.png' },
+    { name: 'Coffe Moranguete', price: 22, src: './img/coffe-caramell.png' },
+    { name: 'Coffe Moranguete', price: 30, src: './img/coffe-chocotino.png' },
+    
+]
